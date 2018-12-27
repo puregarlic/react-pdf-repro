003cd2b87fb7e90db77d97afa659f0c425ac522e
Graham Barber <grahammbarber@gmail.com>
2018-12-27 15:07:54 -0800

    refactor: change to functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { PDFViewer, Document, Font, Page, Text } from '@react-pdf/renderer'
 
 import PTSansCaption from './fonts/PT_Sans-Caption-Web-Regular.ttf'
 
 Font.register(PTSansCaption, { family: 'PT Sans Caption' })
 
-class App extends Component {
-  render() {
-    return (
-      <PDFViewer width="100%" height="100%">
-        <Document>
-          <Page>
-            <Text style={{ fontFamily: 'PT Sans Caption' }}>Hello, world</Text>
-          </Page>
-        </Document>
-      </PDFViewer>
-    )
-  }
-}
+const App = () => (
+  <PDFViewer width="100%" height="100%">
+    <Document>
+      <Page>
+        <Text style={{ fontFamily: 'PT Sans Caption' }}>Hello, world</Text>
+      </Page>
+    </Document>
+  </PDFViewer>
+)
 
 export default App
