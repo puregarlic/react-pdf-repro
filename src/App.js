7f0c138df6f0c07802f1733fc8a2ec28c420b69b
Graham Barber <grahammbarber@gmail.com>
2018-12-27 14:26:27 -0800

    feat: add reproduction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { PDFViewer, Document, Font, Page, Text } from '@react-pdf/renderer'
+
+import PTSansCaption from './fonts/PT_Sans-Caption-Web-Regular.ttf'
+
+Font.register(PTSansCaption, { family: 'PT Sans Caption' })
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <PDFViewer width="100vw" height="100vh">
+        <Document>
+          <Page>
+            <Text style={{ fontFamily: 'PT Sans Caption' }}>Hello, world</Text>
+          </Page>
+        </Document>
+      </PDFViewer>
+    )
   }
 }
 
-export default App;
+export default App
